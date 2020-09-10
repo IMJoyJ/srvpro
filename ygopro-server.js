17032d2df82374e49c43b5a8fa1d606fe0a6a0b4
IMJoyJ <316351048@qq.com>
2020-09-10 13:52:33 +0800

    update js

--- ygopro-server.js
+++ ygopro-server.js
@@ -5382,7 +5382,10 @@
         });
       }
       if (settings.modules.cloud_replay.enabled && settings.modules.tournament_mode.enabled && settings.modules.tournament_mode.replay_safe) {
-        ygopro.stoc_send_chat_to_room(room, `\${cloud_replay_delay_part1}R#${room.cloud_replay_id}\${cloud_replay_delay_part2}`, ygopro.constants.COLORS.BABYBLUE);
+        if (room.sent_cloud_replay === void 0) {
+          room.sent_cloud_replay = true;
+          ygopro.stoc_send_chat_to_room(room, `\${cloud_replay_delay_part1}R#${room.cloud_replay_id}\${cloud_replay_delay_part2}`, ygopro.constants.COLORS.BABYBLUE);
+        }
       }
       return settings.modules.tournament_mode.enabled && settings.modules.tournament_mode.block_replay_to_player || settings.modules.replay_delay && room.hostinfo.mode === 1;
     } else {
